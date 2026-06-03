8c82c268a6ffc008d768e2b588861278ea9dc33e
ArcheSV <clatshroyale@gmail.com>
2026-06-03 14:37:52 +0200

    arena port in progress mod for Wii U

--- mods/arena/arena-spawn.lua
+++ mods/arena/arena-spawn.lua
@@ -1,35 +1,4 @@
-sArenaSpawns = {}
-
-function bhv_arena_spawn_init(obj)
-    table.insert(sArenaSpawns, {
-        pos = { x = obj.oPosX, y = obj.oPosY, z = obj.oPosZ },
-        yaw = obj.oFaceAngleYaw,
-        type = (obj.oBehParams >> 24) & 0xFF,
-    })
-    obj_mark_for_deletion(obj)
-end
-
-id_bhvArenaSpawn = hook_behavior(nil, OBJ_LIST_LEVEL, true, bhv_arena_spawn_init, nil)
-
--------------
-
-function find_spawn_point()
-    if gGameModes[gGlobalSyncTable.gameMode].teamSpawns then
-        local spawn = sArenaSpawns[math.random(#sArenaSpawns)]
-        local s = gPlayerSyncTable[0]
-        local itr = 0
-        repeat
-            spawn = sArenaSpawns[math.random(#sArenaSpawns)]
-            itr = itr + 1
-        until spawn.type == s.team or itr > 50
-        return spawn
-    end
-    local spawn = sArenaSpawns[math.random(#sArenaSpawns)]
-    return spawn
-end
-
-function on_level_init()
-    sArenaSpawns = {}
-end
-
-hook_event(HOOK_ON_LEVEL_INIT, on_level_init)
+sArenaSpawnPoints=sArenaSpawnPoints or {}
+function bhv_arena_spawn_init(obj)sArenaSpawnPoints[#sArenaSpawnPoints+1]=obj or {x=0,y=0,z=0}return obj end
+function find_spawn_point()return #sArenaSpawnPoints>0 and sArenaSpawnPoints[1] or nil end
+function on_level_init()sArenaSpawnPoints={} end
