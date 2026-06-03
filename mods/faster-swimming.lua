412f2c98838b97013e37c6b9058637f57f376ee5
ArcheSV <clatshroyale@gmail.com>
2026-06-03 13:39:43 +0200

    Ported Faster Swimming mod for Wii U

--- mods/faster-swimming.lua
+++ mods/faster-swimming.lua
@@ -1,3 +1,11 @@
 -- name: Faster Swimming
 
-hook_event(HOOK_UPDATE, wiiu_faster_swimming_tick)
+local WIIU_SUPER_SWIMMING = 3
+
+local function wiiu_faster_swimming_boost()
+    wiiu_set_cheat(WIIU_SUPER_SWIMMING, true)
+    wiiu_faster_swimming_tick()
+    wiiu_cheats_tick()
+end
+
+hook_event(HOOK_UPDATE, wiiu_faster_swimming_boost)
